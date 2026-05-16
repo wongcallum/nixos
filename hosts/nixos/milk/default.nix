e1d72ede71907b3b197d47de6f9b43c451d35f27
Callum <mail@callumwong.com>
2026-05-16 14:58:28 +1000

    import modules/virtualisation/digital-ocean-image.nix

--- hosts/nixos/milk/default.nix
+++ hosts/nixos/milk/default.nix
@@ -1,7 +1,9 @@
-{ config, ... }:
+{ inputs, config, ... }:
 {
   flake.modules.nixos."hosts/nixos/milk" = {
     imports = with config.flake.modules.nixos; [
+      "${inputs.nixpkgs}/nixos/modules/virtualisation/digital-ocean-image.nix"
+
       ./_configuration.nix
 
       callum
