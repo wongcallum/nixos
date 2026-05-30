fa6075d84dbb48907e8c7ffa5a916c5dda10aa79
callum <mail@callumwong.com>
2026-05-30 23:02:33 +1000

    add firefox preferences

--- modules/programs/firefox.nix
+++ modules/programs/firefox.nix
@@ -1,34 +1,45 @@
 {
-  flake.modules.nixos.firefox = {
-    programs.firefox = {
-      enable = true;
-      autoConfig = ''
-        // Any comment. You must start the file with a single-line comment!
-        var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
+  flake.modules.nixos.firefox =
+    { config, ... }:
+    {
+      programs.firefox = {
+        enable = true;
+        preferences = {
+          "browser.fixup.domainsuffixwhitelist.${config.modules.gateway.tld}" = true;
+          "browser.download.panel.shown" = true;
+          "browser.search.suggest.enable" = false;
+          "browser.urlbar.suggest.searches" = false;
+          "browser.ml.linkPreview.enabled" = false;
+          "browser.ai.control.default" = "blocked";
+          "browser.urlbar.showSearchTerms.enabled" = true;
+        };
+        autoConfig = ''
+          // Any comment. You must start the file with a single-line comment!
+          var { classes: Cc, interfaces: Ci, utils: Cu } = Components;
 
-        // Set new tab page
-        try {
-          ChromeUtils.importESModule(
-            "resource:///modules/AboutNewTab.sys.mjs",
-          ).AboutNewTab.newTabURL = "https://prism.tower.7sref";
-        } catch (e) {
-          Cu.reportError(e);
-        } // report errors in the Browser Console
+          // Set new tab page
+          try {
+            ChromeUtils.importESModule(
+              "resource:///modules/AboutNewTab.sys.mjs",
+            ).AboutNewTab.newTabURL = "https://prism.tower.7sref";
+          } catch (e) {
+            Cu.reportError(e);
+          } // report errors in the Browser Console
 
-        // Auto focus new tab content
-        try {
-          const { BrowserWindowTracker } = ChromeUtils.importESModule(
-            "resource:///modules/BrowserWindowTracker.sys.mjs",
-          );
-          const Services = globalThis.Services;
-          Services.obs.addObserver((event) => {
-            window = BrowserWindowTracker.getTopWindow();
-            window.gBrowser.selectedBrowser.focus();
-          }, "browser-open-newtab-start");
-        } catch (e) {
-          Cu.reportError(e);
-        }
-      '';
+          // Auto focus new tab content
+          try {
+            const { BrowserWindowTracker } = ChromeUtils.importESModule(
+              "resource:///modules/BrowserWindowTracker.sys.mjs",
+            );
+            const Services = globalThis.Services;
+            Services.obs.addObserver((event) => {
+              window = BrowserWindowTracker.getTopWindow();
+              window.gBrowser.selectedBrowser.focus();
+            }, "browser-open-newtab-start");
+          } catch (e) {
+            Cu.reportError(e);
+          }
+        '';
+      };
     };
-  };
 }
