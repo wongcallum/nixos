2d2a1d46ac2c3d062ab9461299fe6ed9ad1ea8d7
Callum <mail@callumwong.com>
2026-05-31 21:06:57 +1000

    update qalculate commit ref so i won't make anymore noise on nixpkgs [skip ci]

--- scripts/patch-nixpkgs.sh
+++ scripts/patch-nixpkgs.sh
@@ -6,7 +6,7 @@ nixpkgs_rev=$(nix flake metadata --json \
 
 gh-cherry-pick --target wongcallum/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
-  NixOS/nixpkgs/e79de488d0c3e3aa7ec7b644bb3382cd33e98171 \
+  NixOS/nixpkgs/8927bc2ba352c2479125cccbf6f22c9fc5db2de3 \
   wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9
   # ^ include commits, branches, or PRs https://github.com/PerchunPak/ghcherry
 
