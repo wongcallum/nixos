f6d52821eb58f26edd7d04aff8ea9471dddd8286
Callum <mail@callumwong.com>
2026-06-02 14:44:00 +1000

    qalculate-gtk merged to nixos-unstable

--- scripts/patch-nixpkgs.sh
+++ scripts/patch-nixpkgs.sh
@@ -6,7 +6,6 @@ nixpkgs_rev=$(nix flake metadata --json \
 
 gh-cherry-pick --target wongcallum/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
-  NixOS/nixpkgs/8927bc2ba352c2479125cccbf6f22c9fc5db2de3 \
   wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9
   # ^ include commits, branches, or PRs https://github.com/PerchunPak/ghcherry
 
