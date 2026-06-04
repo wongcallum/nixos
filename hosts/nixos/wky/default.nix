ac2c9477cf3949670daa7c3f6549e33a48885ac6
callum <mail@callumwong.com>
2026-06-04 22:57:41 +1000

    wky: nnn-git

--- hosts/nixos/wky/default.nix
+++ hosts/nixos/wky/default.nix
@@ -38,6 +38,20 @@
 
     environment.variables.EDITOR = "nvim";
 
+    nixpkgs.overlays = [
+      (final: prev: {
+        nnn = prev.nnn.overrideAttrs (old: {
+          version = "5.3-unstable-2026-05-29";
+          src = final.fetchFromGitHub {
+            owner = "jarun";
+            repo = "nnn";
+            rev = "2f1d36273ac256723781be82088d6f95edbbe2e5";
+            sha256 = "sha256-u77QZOlzLZ4CDjZmuGnyEF9avOoMbLxnRO7M2JHTb1g=";
+          };
+        });
+      })
+    ];
+
     networking.networkmanager.enable = true;
     services.resolved.enable = true;
     documentation.man.cache.enable = false;
