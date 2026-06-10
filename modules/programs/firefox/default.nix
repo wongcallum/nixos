3088f4d82370ec329a88a686153040bb938664f0
callum <mail@callumwong.com>
2026-06-10 14:04:56 +1000

    firefox userChrome

--- modules/programs/firefox/default.nix
+++ modules/programs/firefox/default.nix
@@ -13,6 +13,8 @@
             "browser.ml.linkPreview.enabled" = false;
             "browser.ai.control.default" = "blocked";
             "browser.urlbar.showSearchTerms.enabled" = true;
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            "browser.uidensity" = 1; # compact
           }
           (lib.optionalAttrs config.modules.fonts.enable {
             "font.name.serif.x-western" = "HarmonyOS Sans TC";
@@ -43,6 +45,18 @@
             }, "browser-open-newtab-start");
           } catch (e) {
             Cu.reportError(e);
+          }
+
+          // Load userChrome.css
+          try {
+            let sss = Cc["@mozilla.org/content/style-sheet-service;1"].getService(Ci.nsIStyleSheetService);
+            let ios = Cc["@mozilla.org/network/io-service;1"].getService(Ci.nsIIOService);
+            let uri = ios.newURI("file://${./userChrome.css}");
+            if (!sss.sheetRegistered(uri, sss.USER_SHEET)) {
+              sss.loadAndRegisterSheet(uri, sss.USER_SHEET);
+            }
+          } catch (ex) {
+            Cu.reportError(ex.message);
           }
         '';
       };
