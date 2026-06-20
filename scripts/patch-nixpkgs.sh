7e51b004d7ce192769d8dfaf5dc53b55e08d908d
callum <mail@callumwong.com>
2026-06-20 22:03:24 +1000

    patch nixpkgs with PR #532439 to upgrade usage

--- scripts/patch-nixpkgs.sh
+++ scripts/patch-nixpkgs.sh
@@ -6,7 +6,8 @@ nixpkgs_rev=$(nix flake metadata --json \
 
 gh-cherry-pick --target wongcallum/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
-  wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9
+  wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9 \
+  NixOS/nixpkgs/df11c04eafb13f1f2913c198a24d603f9a7b2db9
   # ^ include commits, branches, or PRs https://github.com/PerchunPak/ghcherry
 
 nix flake update unstable
