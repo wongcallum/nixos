1744e8dda99048ace958ae16847523d7b44d6803
callum <mail@callumwong.com>
2026-06-21 22:28:56 +1000

    Revert "ghostty run service at login"
    This reverts commit 122178d8ba245fa93558d41669de77e69c6e35a8.

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -2,18 +2,9 @@
 {
   flake.modules.nixos.ghostty =
     { pkgs, ... }:
-    let
-      ghostty = inputs.ghostty.packages.${pkgs.system}.default;
-    in
     {
-      environment.systemPackages = [ ghostty ];
-
-      systemd.packages = [ ghostty ];
-
-      # warm first start :)
-      systemd.user.services."app-com.mitchellh.ghostty" = {
-        overrideStrategy = "asDropin";
-        wantedBy = [ "graphical-session.target" ];
-      };
+      environment.systemPackages = [
+        inputs.ghostty.packages.${pkgs.system}.default
+      ];
     };
 }
