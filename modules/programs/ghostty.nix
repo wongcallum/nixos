122178d8ba245fa93558d41669de77e69c6e35a8
Callum <mail@callumwong.com>
2026-06-21 19:59:14 +1000

    ghostty run service at login

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -2,9 +2,18 @@
 {
   flake.modules.nixos.ghostty =
     { pkgs, ... }:
+    let
+      ghostty = inputs.ghostty.packages.${pkgs.system}.default;
+    in
     {
-      environment.systemPackages = [
-        inputs.ghostty.packages.${pkgs.system}.default
-      ];
+      environment.systemPackages = [ ghostty ];
+
+      systemd.packages = [ ghostty ];
+
+      # warm first start :)
+      systemd.user.services."app-com.mitchellh.ghostty" = {
+        overrideStrategy = "asDropin";
+        wantedBy = [ "graphical-session.target" ];
+      };
     };
 }
