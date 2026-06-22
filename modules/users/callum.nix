cafa430fd69832cf2aec0c594ac4796b9697a1a3
callum <mail@callumwong.com>
2026-06-22 21:27:31 +1000

    add root@wky key for remote builds (temporary)

--- modules/users/callum.nix
+++ modules/users/callum.nix
@@ -17,6 +17,7 @@
             openssh.authorizedKeys.keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMP4bm4SjbUcveDfeNVU7QkWz/pFWuVrPsZIa5e6ZE64 callum@acid"
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICDCd6wji8oxyVnqDq3I5qMTOfNJ8Z/Rm/eGfismjUL4 callum@wky"
+              "no-pty ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+hBUksJQH8d/bXoDfZ+AzohvjBNM8CHZS9SqEApeiE root@wky"
             ];
             extraGroups = lib.mkMerge [
               (lib.optionals config.virtualisation.libvirtd.enable [ "libvirtd" ])
