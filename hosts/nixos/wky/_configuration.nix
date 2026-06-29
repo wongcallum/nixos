212f346a05a60672039b4750817cab09d0b12905
callum <mail@callumwong.com>
2026-06-29 10:24:38 +1000

    limine font

--- hosts/nixos/wky/_configuration.nix
+++ hosts/nixos/wky/_configuration.nix
@@ -4,9 +4,37 @@
   lib,
   ...
 }:
+let
+  aixoid9-f16 = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/viler-int10h/vga-text-mode-fonts/ab3965599eb3d0de9834fa9c26942b4e4843a42b/FONTS/AIXOID9.F16";
+    hash = "sha256-+nOBq3pTPm74LnJNTPcwYNwC4befR/EBa7NkAiOxBaQ=";
+  };
+in
 {
   boot = {
-    loader.limine.enable = true;
+    loader = {
+      timeout = 10;
+      limine = {
+        enable = true;
+
+        additionalFiles."fonts/AIXOID9.F16" = aixoid9-f16;
+        extraConfig = "term_font: boot():/limine/fonts/AIXOID9.F16";
+
+        # theme from https://github.com/diegons490/cachyos-limine-theme
+        style = {
+          wallpapers = [ pkgs.nixos-artwork.wallpapers.nineish-catppuccin-mocha-alt.gnomeFilePath ];
+          interface.branding = "wky";
+          graphicalTerminal = {
+            palette = "1e1e2e;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
+            brightPalette = "585b70;f38ba8;a6e3a1;f9e2af;89b4fa;f5c2e7;94e2d5;cdd6f4";
+            background = "ffffffff";
+            foreground = "cdd6f4";
+            brightBackground = "ffffffff";
+            brightForeground = "cdd6f4";
+          };
+        };
+      };
+    };
 
     kernelModules = [ "kvm-intel" ];
     initrd.availableKernelModules = [
