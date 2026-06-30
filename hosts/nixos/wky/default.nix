e0484ad969f309409804fd55265f3147b8d8c3af
callum <mail@callumwong.com>
2026-07-01 09:43:39 +1000

    set GOPATH and GOBIN

--- hosts/nixos/wky/default.nix
+++ hosts/nixos/wky/default.nix
@@ -31,7 +31,11 @@
       zoxide
     ]);
 
-    environment.variables.EDITOR = "nvim";
+    environment.variables = {
+      EDITOR = "nvim";
+      GOPATH = "~/.local/share/go";
+      GOBIN = "~/.local/bin";
+    };
 
     nixpkgs.overlays = [
       (final: prev: {
