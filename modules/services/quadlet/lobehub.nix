38e3810817c8466b080052185c6c6911c08dab08
Callum <mail@callumwong.com>
2026-07-01 00:26:19 +1000

    wire lobehub sandbox to onlyboxes provider

--- modules/services/quadlet/lobehub.nix
+++ modules/services/quadlet/lobehub.nix
@@ -151,6 +151,12 @@ in
                   S3_SECRET_ACCESS_KEY = "minioadmin";
                   LLM_VISION_IMAGE_USE_BASE64 = "1";
 
+                  # self-hosted OnlyBoxes sandbox (see ./onlyboxes.nix). The
+                  # console shares the lobehub network at 172.28.0.6. The
+                  # matching ONLYBOXES_JIT_SIGNING_KEY lives in the sops env file.
+                  SANDBOX_PROVIDER = "onlyboxes";
+                  ONLYBOXES_BASE_URL = "http://172.28.0.6:8089";
+
                   # api keys defined in nixos-secrets
                   SEARCH_PROVIDERS = "tavily,exa";
                   CRAWLER_IMPLS = "exa,naive";
