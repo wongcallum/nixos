11a345b2da24a1f918cfe6724a7d9b0abe978be2
callum <mail@callumwong.com>
2026-07-02 02:45:27 +1000

    replace wky ssh key with shama's

--- modules/users/callum.nix
+++ modules/users/callum.nix
@@ -16,8 +16,7 @@
             initialPassword = "changeme";
             openssh.authorizedKeys.keys = [
               "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMP4bm4SjbUcveDfeNVU7QkWz/pFWuVrPsZIa5e6ZE64 callum@acid"
-              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICDCd6wji8oxyVnqDq3I5qMTOfNJ8Z/Rm/eGfismjUL4 callum@wky"
-              "no-pty ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIH+hBUksJQH8d/bXoDfZ+AzohvjBNM8CHZS9SqEApeiE root@wky"
+              "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIInBiS3lc/8BUJLibu1+6KSu+pEOLXPCRxY/FLF5GMo5 callum@shama"
             ];
             extraGroups = lib.mkMerge [
               (lib.optionals config.virtualisation.libvirtd.enable [ "libvirtd" ])
