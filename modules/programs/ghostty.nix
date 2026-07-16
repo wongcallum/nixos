9febfe19ceea9380ee1fc8f6a6f85ec7efae5f14
callum <mail@callumwong.com>
2026-07-16 19:26:57 +1000

    temporarily patch ghostty open drain busy loop

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -1,10 +1,24 @@
 { inputs, ... }:
+let
+  # prevent CPU spike and journal flood due to bug described in discussion #13267
+  patchGhostty =
+    ghostty:
+    ghostty.overrideAttrs (old: {
+      patches = (old.patches or [ ]) ++ [ ./ghostty-open-drain-busyloop.patch ];
+    });
+in
 {
+  perSystem =
+    { system, ... }:
+    {
+      packages.ghostty = patchGhostty inputs.ghostty.packages.${system}.default;
+    };
+
   flake.modules.nixos.ghostty =
     { pkgs, ... }:
     {
       environment.systemPackages = [
-        inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
+        (patchGhostty inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default)
       ];
     };
 }
