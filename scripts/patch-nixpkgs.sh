a321511c62efcfa77c9fdd3e0bb6334db561e488
callum <mail@callumwong.com>
2026-07-16 16:41:19 +1000

    replace deprecated gh-cherry-pick with ghcherry

--- scripts/patch-nixpkgs.sh
+++ scripts/patch-nixpkgs.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env -S nix shell nixpkgs#gh-cherry-pick nixpkgs#jq --command bash
+#!/usr/bin/env -S nix shell nixpkgs#ghcherry nixpkgs#jq --command bash
 set -euo pipefail
 
 nixpkgs_rev=$(nix flake metadata --json \
   | jq -er '.locks.nodes."unstable-upstream".locked.rev')
 
-gh-cherry-pick --target wongcallum/nixpkgs@patched \
+ghcherry --target wongcallum/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
   wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9
   # ^ include commits, branches, or PRs https://github.com/PerchunPak/ghcherry
