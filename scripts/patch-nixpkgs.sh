519013773a34f8cce95be55a116dcccf0237ed4e
Callum Wong <mail@callumwong.com>
2026-07-27 23:07:14 +1000

    apply nixpkgs#546004 to fix niri build problem
    "libdisplay-info{,_0_2}: make generic, libdisplay-info_0_3: init at
    0.3.0, niri: pin libdisplay-info_0_3"

--- scripts/patch-nixpkgs.sh
+++ scripts/patch-nixpkgs.sh
@@ -6,7 +6,8 @@ nixpkgs_rev=$(nix flake metadata --json \
 
 ghcherry --target wongcallum/nixpkgs@patched \
   --first-hard-reset-to "NixOS/nixpkgs/$nixpkgs_rev" \
-  wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9
+  wongcallum/nixpkgs/6532eb6b9d9afa0911e333ee197eb30d9716fdf9 \
+  NixOS/nixpkgs#546004
   # ^ include commits, branches, or PRs https://github.com/PerchunPak/ghcherry
 
 nix flake update unstable
