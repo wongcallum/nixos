b017e84edd434db75cfbfa1cde308603c2d8d667
Callum Wong <mail@callumwong.com>
2026-07-29 01:55:21 +1000

    fix nix-monitored shell completion

--- modules/nix-monitored.nix
+++ modules/nix-monitored.nix
@@ -1,9 +1,18 @@
 { inputs, ... }:
 {
-  flake.modules.nixos.nix-monitored = {
-    imports = [ inputs.nix-monitored.nixosModules.default ];
+  flake.modules.nixos.nix-monitored =
+    { pkgs, ... }:
+    {
+      imports = [ inputs.nix-monitored.nixosModules.default ];
 
-    nix.monitored.enable = true;
-    nix.monitored.notify = false;
-  };
+      nix.monitored = {
+        enable = true;
+        notify = false;
+        package = pkgs.nix-monitored.overrideAttrs (old: {
+          patches = (old.patches or [ ]) ++ [
+            ../patches/nix-monitored-completions.patch
+          ];
+        });
+      };
+    };
 }
