e5b7df5e98257670c85e7785e685dc324363b09c
callum <mail@callumwong.com>
2026-08-05 17:10:21 +1000

    fix llama CUDA service dependency and set cpu offload to 37 layers

--- modules/services/llama-cpp.nix
+++ modules/services/llama-cpp.nix
@@ -1,6 +1,11 @@
 {
   flake.modules.nixos.llama-cpp =
-    { lib, pkgs, ... }:
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
     {
       services.llama-cpp = {
         enable = true;
@@ -12,7 +17,7 @@
           hf-repo = "unsloth/Qwen3.6-35B-A3B-MTP-GGUF:UD-Q6_K_XL";
           alias = "qwen3.6-35b-a3b";
 
-          n-cpu-moe = 64;
+          n-cpu-moe = 37;
           n-gpu-layers = 99;
           kv-offload = true;
           threads = 12;
@@ -36,11 +41,12 @@
       };
 
       systemd.services.llama-cpp.unitConfig = {
-        Requires = [ "dev-nvidia1.device" ];
-        After = [ "dev-nvidia1.device" ];
+        Wants = [ "systemd-modules-load.service" ];
+        After = [ "systemd-modules-load.service" ];
       };
 
       systemd.services.llama-cpp.serviceConfig = {
+        ExecStartPre = "${pkgs.bash}/bin/bash -c 'until ${config.hardware.nvidia.package.bin}/bin/nvidia-smi -L >/dev/null 2>&1; do ${pkgs.coreutils}/bin/sleep 1; done'";
         Environment = lib.mkForce [
           "CUDA_VISIBLE_DEVICES=GPU-41a667ec-3e58-e64c-1eeb-fb916f0b286f"
           "LLAMA_CACHE=/var/lib/llama-cpp/cache"
