a946299ec8b6ba4eea6f6a010168ceeb3bb945d4
Callum Wong <mail@callumwong.com>
2026-08-18 10:56:45 +1000

    remove nnn override

--- hosts/nixos/shama/default.nix
+++ hosts/nixos/shama/default.nix
@@ -67,19 +67,6 @@ in
       };
 
       nixpkgs.config.allowUnfree = true;
-      nixpkgs.overlays = [
-        (final: prev: {
-          nnn = prev.nnn.overrideAttrs {
-            version = "5.3-unstable-2026-05-29";
-            src = final.fetchFromGitHub {
-              owner = "jarun";
-              repo = "nnn";
-              rev = "2f1d36273ac256723781be82088d6f95edbbe2e5";
-              sha256 = "sha256-u77QZOlzLZ4CDjZmuGnyEF9avOoMbLxnRO7M2JHTb1g=";
-            };
-          };
-        })
-      ];
 
       networking.networkmanager.enable = true;
       services.resolved.enable = true;
