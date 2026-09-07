0dd6fcff3e31a4bb1ac81d04c0cac279c9872ec4
Callum Wong <mail@callumwong.com>
2026-09-07 15:45:32 +1000

    ghostty bug fixed in #14125 :tada:

--- modules/programs/ghostty.nix
+++ modules/programs/ghostty.nix
@@ -1,24 +1,17 @@
 { inputs, ... }:
-let
-  # prevent CPU spike and journal flood due to bug described in discussion #13267
-  patchGhostty =
-    ghostty:
-    ghostty.overrideAttrs (old: {
-      patches = (old.patches or [ ]) ++ [ ./ghostty-open-drain-busyloop.patch ];
-    });
-in
+
 {
   perSystem =
     { system, ... }:
     {
-      packages.ghostty = patchGhostty inputs.ghostty.packages.${system}.default;
+      packages.ghostty = inputs.ghostty.packages.${system}.default;
     };
 
   flake.modules.nixos.ghostty =
     { pkgs, ... }:
     {
       environment.systemPackages = [
-        (patchGhostty inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default)
+        inputs.ghostty.packages.${pkgs.stdenv.hostPlatform.system}.default
       ];
     };
 }
